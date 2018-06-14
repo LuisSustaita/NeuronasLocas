12c9c1133922deab41f3e3bdf7d87dfb3a31202b
LuisSustaita <ld.delacruzsustaita@ugto.mx>
2018-06-14 13:26:47 -0500

    Se agregaron comentarios

--- pruebas.py
+++ pruebas.py
@@ -1,27 +1,37 @@
 #http://webs.ucm.es/info/aocg/python/modulos_cientificos/matplotlib/index.html
+#Librerias para graficar
 import seaborn as sns
 import matplotlib.pyplot as plt
+#Clase Neurona
 from Neurona import neurona
 
-n1=neurona(2,0.5,1,3)
-n2=neurona(5,0.5,1.5,5)
+#Iniciamos 2 neuronas
+n1=neurona(FiringTime=2,Weigh=0.5,Delay=1,Tau=3)
+n2=neurona(FiringTime=5,Weigh=0.5,Delay=1.5,Tau=5)
 
+#Las agregamos a una lista de neuronas
 NeuronList=[n1,n2]
 
+#Definimos una funcion para obtener Ui(t)
 def TotalUi(NeuronList,ActualTime):
     result=0
     for neuron in NeuronList:
         result+=neuron.Ui(ActualTime)
     return result
 
+#Definimos listas para graficar los resultados de cada neurona
 UiN1List=[]
 UiN2List=[]
+#Definimos una lista para graficar Ui(t)
 UiList=[]
+
+#Iniciamos el 'tiempo'
 for i in range(21):
+    #Agregamos a las listas los totales de las operaciones
     UiList.append(TotalUi(NeuronList,i))
     UiN1List.append(n1.Ui(i))
     UiN2List.append(n2.Ui(i))
-    
+
     '''
     print("Actual Time: "+str(i))
     print("Neurona 1:")
@@ -34,6 +44,7 @@ for i in range(21):
     print("Ui2: "+str(n2.Ui(i)))
     '''
 
+#Graficamos
 plt.figure()
 plt.plot(UiN1List,label="Ui de Neurona 1")
 plt.plot(UiN2List,label="Ui de Neurona 2")
