5848f3a03dadeb6af05e34a674710d243dd215d3
LuisSustaita <ld.delacruzsustaita@ugto.mx>
2018-06-15 15:24:27 -0500

    Se agrega un limite para el disparo

--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,15 @@
 #http://webs.ucm.es/info/aocg/python/modulos_cientificos/matplotlib/index.html
 #Librerias para graficar
-import seaborn as sns
 import matplotlib.pyplot as plt
+
 #Clase Neurona
 from Neurona import neurona
 
 #Iniciamos 2 neuronas
 n1=neurona(FiringTime=2,Weigh=0.5,Delay=1,Tau=3)
 n2=neurona(FiringTime=5,Weigh=0.5,Delay=1.5,Tau=5)
+
+Threshold=0.8
 
 #Las agregamos a una lista de neuronas
 NeuronList=[n1,n2]
@@ -24,30 +26,33 @@ UiN1List=[]
 UiN2List=[]
 #Definimos una lista para graficar Ui(t)
 UiList=[]
-
 #Iniciamos el 'tiempo'
-for i in range(21):
-    #Agregamos a las listas los totales de las operaciones
-    UiList.append(TotalUi(NeuronList,i))
-    UiN1List.append(n1.Ui(i))
-    UiN2List.append(n2.Ui(i))
+for i in range(100):
+    if TotalUi(NeuronList,i)>Threshold:
+        i=0
+    else:
+        #Agregamos a las listas los totales de las operaciones
+        UiList.append(TotalUi(NeuronList,i))
+        UiN1List.append(n1.Ui(i))
+        UiN2List.append(n2.Ui(i))
 
-    '''
-    print("Actual Time: "+str(i))
-    print("Neurona 1:")
-    print("Arg: "+str(n1.Eargs(i)))
-    print("Y: "+str(n1.Ye(i)))
-    print("Ui1: "+str(n1.Ui(i)))
-    print("Neurona 2:")
-    print("Arg: "+str(n2.Eargs(i)))
-    print("Y: "+str(n2.Ye(i)))
-    print("Ui2: "+str(n2.Ui(i)))
-    '''
+        '''
+        print("Actual Time: "+str(i))
+        print("Neurona 1:")
+        print("Arg: "+str(n1.Eargs(i)))
+        print("Y: "+str(n1.Ye(i)))
+        print("Ui1: "+str(n1.Ui(i)))
+        print("Neurona 2:")
+        print("Arg: "+str(n2.Eargs(i)))
+        print("Y: "+str(n2.Ye(i)))
+        print("Ui2: "+str(n2.Ui(i)))
+        '''
 
 #Graficamos
 plt.figure()
 plt.plot(UiN1List,label="Ui de Neurona 1")
 plt.plot(UiN2List,label="Ui de Neurona 2")
 plt.plot(UiList,label="Ui")
+plt.title('Spikes')
 
 plt.show()
