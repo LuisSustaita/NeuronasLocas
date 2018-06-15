0eee04a51d602224eb1c32544b950bfb4383f6ad
LuisSustaita <ld.delacruzsustaita@ugto.mx>
2018-06-15 17:47:19 -0500

    Actualizacion de FiringTime en funciones

--- Neurona.py
+++ Neurona.py
@@ -1,19 +1,18 @@
 class neurona:
-    def __init__(self,FiringTime=[],Weigh,Delay,Tau):
+    def __init__(self,FiringTime,Weigh,Delay,Tau):
         self.FiringTime=FiringTime[:]
         self.Weigh=Weigh
         self.Delay=Delay
         self.Tau=Tau
 
-    def Eargs(self,ActualTime):
-        for FiTi in FiringTime:
-            Yj+=ActualTime-self.FiTi-self.Delay
+    def Eargs(self,ActualTime,i):
+        Yj=ActualTime-self.FiringTime[i]-self.Delay
         return Yj if Yj>0 else 0
 
-    def Ye(self,ActualTime):
+    def Ye(self,ActualTime,i):
         import numpy as np
-        op=self.Eargs(ActualTime)/self.Tau
+        op=self.Eargs(ActualTime,i)/self.Tau
         return op*np.exp(1-op)
 
-    def Ui(self,ActualTime):
-        return self.Ye(ActualTime)*self.Weigh
+    def Ui(self,ActualTime,i):
+        return self.Ye(ActualTime,i)*self.Weigh
